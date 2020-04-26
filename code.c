c4453f83497909725bd24bbf329c042600dc5eed
Admin <62583519+TonyForceField@users.noreply.github.com>
2020-04-26 18:58:17 +0800

    Update code.c
    Added ability to tell time in code.c

--- code.c
+++ code.c
@@ -1,1 +1,15 @@
 // Your C code here
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    
+    time_t currentTime;
+    time(&currentTime);
+    
+    struct tm* utcTime = localtime(&currentTime);
+    printf("Hello wonderful person, the current time is %s!", asctime(utcTime));
+
+    return 0;
+}
