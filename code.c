3928427df229f9cccba6a3841b2c27332a1a3fb7
maxloo2 <maxlo1999112@hotmail.com>
2020-04-28 14:47:35 +0800

    fixed small bug in code.c
    removed unwanted '\n' when printing to terminal console

--- code.c
+++ code.c
@@ -1,15 +1,22 @@
-// Your C code here
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 int main()
 {
-    
+
     time_t currentTime;
     time(&currentTime);
-    
-    struct tm* utcTime = localtime(&currentTime);
-    printf("Hello wonderful person, the current time is %s!", asctime(utcTime));
+
+    struct tm *utcTime = localtime(&currentTime);
+
+    // asctime(utcTime) will result in trailing '\n'
+    // we can use strtok to remove the newline character
+
+    char *time = asctime(utcTime);
+    strtok(time, "\n");
+
+    printf("Hello wonderful person, the current time is %s!", time);
 
     return 0;
 }
